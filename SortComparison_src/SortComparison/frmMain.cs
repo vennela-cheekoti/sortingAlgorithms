3a0f600b671ad0f7441a7207b70f6c47c1a84490
Susritha <susrithagade1996@>
2019-02-06 17:01:26 -0600

    final commit

--- SortComparison_src/SortComparison/frmMain.cs
+++ SortComparison_src/SortComparison/frmMain.cs
@@ -38,6 +38,7 @@ namespace SortComparison
             this.cboAlg2.BackColor=Color.Green;
             this.cmdShuffle.BackColor=Color.GreenYellow;
             this.txtOutputFolder.ForeColor=Color.Red;
+            this.chkAnimation.ForeColor = Color.Green;
             this.InitializeOutputFolder();
             this.cmdShuffle.PerformClick();
 
